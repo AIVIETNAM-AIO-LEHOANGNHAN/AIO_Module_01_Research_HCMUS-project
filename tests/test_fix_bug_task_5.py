f404ca24f20c85c3c4476a5b43102539f184d2c7
AIVIETNAM-AIO-LEHOANGNHAN <lehoangnhan3062006.nvt@gmail.com>
2026-06-19 17:07:13 +0700

    Update test_fix_bug_task_5.py

--- tests/test_fix_bug_task_5.py
+++ tests/test_fix_bug_task_5.py
@@ -1,13 +1,16 @@
 import json
 
 def test_stopwords_exist_in_vocab():
-    
     with open("data/stopwords/custom.txt", encoding="utf-8") as f:
         stopwords = set(line.strip() for line in f if line.strip())
 
     with open("models/vocab/pos_vocab.json", encoding="utf-8") as f:
         pos_vocab = json.load(f)
 
-    overlap = stopwords.intersection(pos_vocab.keys())
+    with open("models/vocab/neg_vocab.json", encoding="utf-8") as f:
+        neg_vocab = json.load(f)
 
-    assert len(overlap) > 0
+    pos_overlap = stopwords.intersection(pos_vocab.keys())
+    neg_overlap = stopwords.intersection(neg_vocab.keys())
+
+    assert len(pos_overlap) > 0 or len(neg_overlap) > 0
