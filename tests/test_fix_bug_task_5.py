1922755d8cc0e362ea8f101e603ad199ca006f2e
AIVIETNAM-AIO-LEHOANGNHAN <lehoangnhan3062006.nvt@gmail.com>
2026-06-19 15:40:54 +0700

    Update test_fix_bug_task_5.py

--- tests/test_fix_bug_task_5.py
+++ tests/test_fix_bug_task_5.py
@@ -1,21 +1,13 @@
 import json
-from utils.text_utils import load_stopwords
-
 
 def test_stopwords_exist_in_vocab():
-    stopwords = load_stopwords(
-        "data/stopwords/custom.txt"
-    )
-  
-    with open(
-        "models/vocab/pos_vocab.json",
-        encoding="utf-8"
-    ) as f:
+    
+    with open("data/stopwords/custom.txt", encoding="utf-8") as f:
+        stopwords = set(line.strip() for line in f if line.strip())
+
+    with open("models/vocab/pos_vocab.json", encoding="utf-8") as f:
         pos_vocab = json.load(f)
 
-    overlap = stopwords.intersection(
-        pos_vocab.keys()
-    )
+    overlap = stopwords.intersection(pos_vocab.keys())
 
-    # Ít nhất phải có một stopword xuất hiện
     assert len(overlap) > 0
